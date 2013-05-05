555f2ea32140612d5a77f12b5383ad9ca5ad0c89
ismc2222 <admin@infonetlive.com>
2013-05-05 18:48:10 +0100

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
-	describe "Static pages" do
-		describe "Home page" do
-			it "should have the h1 'Sample App'" do
-				visit '/static_pages/home'
-				page.should have_selector('h1', :text => 'Sample App')
-			end
-			it "should have the title 'Home'" do
-				visit '/static_pages/home'
-				page.should have_selector('title',:text => "Fibromyalgia Trust | Home")
-			end
-		end
+describe "Static pages" do
+describe "Home page" do
+it "should have the h1 'Sample App'" do
+visit '/static_pages/home'
+page.should have_selector('h1', :text => 'Sample App')
+end
+it "should have the base title" do
+visit '/static_pages/home'
+page.should have_selector('title',
+:text => "Fibromyalgia Trust")
+end
+it "should not have a custom page title" do
+visit '/static_pages/home'
+page.should_not have_selector('title', :text => '| Home')
+end
+end
 		describe "Help page" do
 			it "should have the h1 'Help'" do
 				visit '/static_pages/help'
@@ -28,6 +33,16 @@ require 'spec_helper'
 			it "should have the title 'About Us'" do
 				visit '/static_pages/about'
 				page.should have_selector('title',:text => "Fibromyalgia Trust | About Us")
+			end
+		end
+		describe "Contact page" do
+			it "should have the h1 'Contact'" do
+				visit '/static_pages/contact'
+				page.should have_selector('h1', :text => 'Contact')
+			end
+			it "should have the title 'Contact'" do
+				visit '/static_pages/contact'
+				page.should have_selector('title',:text => "Fibromyalgia Trust | Contact")
 			end
 		end
 	end
